cc5537c9e66c7006733b1b101c090f7f82e1a01f
Valentin-gastonberger <91471804+Valentin-gastonberger@users.noreply.github.com>
2021-11-29 16:56:25 +0100

    test

--- site/vue/pageConnexion.php
+++ site/vue/pageConnexion.php
@@ -2,8 +2,22 @@
 <html lang="fr">
     <head>
         <title>Page de connexion</title>
+        <link href="css/base.css" rel="stylesheet">
+        <link href="css/connexion.css" rel="stylesheet">
     </head>
     <body>
-        <h1>TEST</h1>
+        <div class="container">
+            <div class="tete"></div>
+            <form action="vue/pageMenu.php" method="POST">
+                <div class="content">
+                    <h1>CashCash connexion</h1>
+                    <input type="text" name="id" id="id" tabindex="1" placeholder="Identifiant" required>
+                    <input type="password" name="mdp" idx²="mdp" tabindex="2" placeholder="Mot de passe" required>
+                </div>
+                <div class="login">
+                    <input type="submit" tabindex="3" value="Connexion">
+                </div>
+            </form>
+        </div>
     </body>
 </html>
---
+++ site/vue/pageMenu.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Page menu</title>
+        <link href="css/base.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Page menu</h1>
+    </body>
+</html>
