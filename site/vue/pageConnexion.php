481540c5f9207e81a809f407821876ebb35289a5
Leo <debonnetleo4@gmail.com>
2021-11-29 17:20:22 +0100

    la der des der d'ajd

--- site/vue/pageConnexion.php
+++ site/vue/pageConnexion.php
@@ -13,8 +13,8 @@
                 <div class="content">
                     <h1>CashCash connexion</h1>
                     <div class="inputs">
-                        <input type="text" name="id" id="id" tabindex="1" placeholder="Identifiant">
-                        <input type="password" name="mdp" id="mdp" tabindex="2" placeholder="Mot de passe">
+                        <input type="text" name="id" id="id" tabindex="1" placeholder="Identifiant" required>
+                        <input type="password" name="mdp" id="mdp" tabindex="2" placeholder="Mot de passe" required>
                     </div>
                 </div>
                 <div class="login">
