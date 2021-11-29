52f8141d31043c29dc0340efe2a7195a9f106d60
Valentin-gastonberger <91471804+Valentin-gastonberger@users.noreply.github.com>
2021-11-29 17:04:41 +0100

    vers page menu

--- site/vue/pageConnexion.php
+++ site/vue/pageConnexion.php
@@ -2,8 +2,8 @@
 <html lang="fr">
     <head>
         <title>Page de connexion</title>
-        <link href="css/base.css" rel="stylesheet">
-        <link href="css/connexion.css" rel="stylesheet">
+        <link href="css/base.css?v=<?php echo time(); ?>" rel="stylesheet">
+        <link href="css/connexion.css?v=<?php echo time(); ?>" rel="stylesheet">
     </head>
     <body>
         <div class="container">
@@ -11,8 +11,8 @@
             <form action="vue/pageMenu.php" method="POST">
                 <div class="content">
                     <h1>CashCash connexion</h1>
-                    <input type="text" name="id" id="id" tabindex="1" placeholder="Identifiant" required>
-                    <input type="password" name="mdp" idx²="mdp" tabindex="2" placeholder="Mot de passe" required>
+                    <input type="text" name="id" id="id" tabindex="1" placeholder="Identifiant">
+                    <input type="password" name="mdp" id="mdp" tabindex="2" placeholder="Mot de passe">
                 </div>
                 <div class="login">
                     <input type="submit" tabindex="3" value="Connexion">
--- site/vue/pageMenu.php
+++ site/vue/pageMenu.php
@@ -2,9 +2,16 @@
 <html lang="fr">
     <head>
         <title>Page menu</title>
-        <link href="css/base.css" rel="stylesheet">
+        <link href="../css/base.css?v=<?php echo time(); ?>" rel="stylesheet">
+        <link href="../css/nav.css?v=<?php echo time(); ?>" rel="stylesheet">
     </head>
     <body>
+        <nav>
+            <ul>
+                <li>Salut</li>
+                <li>Connecté</li>
+            </ul>
+        </nav>
         <h1>Page menu</h1>
     </body>
 </html>
