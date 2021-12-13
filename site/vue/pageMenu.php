a38341d608df290c9366e105bb92df1d2afae8e7
THYtoon72 <luca.roba@gastonberger.fr>
2021-12-13 16:49:25 +0100

    ui

--- site/vue/pageMenu.php
+++ site/vue/pageMenu.php
@@ -1,9 +1,17 @@
 <body>
     <nav>
         <ul>
-            <li>Salut</li>
-            <li>Connecté</li>
+            <li>CashCash</li>
+            <li>Gestionnaire</li>
         </ul>
     </nav>
     <h1>Page menu</h1>
+    
+    <button class="Gestionnaire" type="button"> Outil Statistique
+    </button>
+
+    <button class="Gestionnaire" type="button"> Affectation Visite
+    </button>
+
+
 </body>
