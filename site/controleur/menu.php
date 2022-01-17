3736cc69684736be66e9ccd457c61391d96e0276
Leo <debonnetleo4@gmail.com>
2022-01-17 14:22:42 +0100

    c bon

--- site/controleur/menu.php
+++ site/controleur/menu.php
@@ -10,8 +10,8 @@ var_dump($travail["matricule"]);
 
     if(isset($_SESSION["matricule"]) && $_SESSION["matricule"] != $travail["matricule"]){  // nom d'utilisateur vérification
         include "$racine/vue/entete.html.php";
-        include "$racine/pageMenu.php";
-        include "$racine/pied.html.php";
+        include "$racine/vue/pageMenu.php";
+        include "$racine/vue/pied.html.php";
         for($i = 0; $i<count($travail); $i++){
             
         }
