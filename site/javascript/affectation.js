377ff6a891dab61e2bc591d04fb908ae7a7af384
Leo <debonnetleo4@gmail.com>
2022-01-17 15:51:34 +0100

    correction bug entete

--- site/javascript/affectation.js
+++ site/javascript/affectation.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     $(".arrowDeploy").on("click", function(){
-        
+        tmp = $(this).index();
+
+        $(".hiddenForm:eq("+tmp+")").css({
+            transform: "scale(1)",
+            height: "auto"
+        });
     });
 });
