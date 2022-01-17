b42d054d598f739cc73e6b6827b1c0d328537864
Valentin-gastonberger <91471804+Valentin-gastonberger@users.noreply.github.com>
2022-01-17 14:49:26 +0100

    page connexion fini

--- site/controleur/verification.php
+++ site/controleur/verification.php
@@ -2,6 +2,8 @@
 // connexion à la base de données
 include_once "../modele/bd.utilisateur.inc.php";
 
+session_start();
+
 if(isset($_POST['matricule']) && isset($_POST['mdp']))
 {      
     $result = verifyPassword($_POST['matricule'], $_POST['mdp']);
@@ -14,22 +16,21 @@ if(isset($_POST['matricule']) && isset($_POST['mdp']))
     if($_POST['matricule'] !== "" && $_POST['mdp'] !== ""){  
         if($_POST['matricule'] == $username && $password == $passwordverif) // nom d'utilisateur et mot de passe correctes
         {
-            session_start();
             $_SESSION['matricule'] = $username;
             header('Location: ./?action=menu');
         }
         else
         {
-           header('Location: ./?action=connexion?erreur=1'); // utilisateur ou mot de passe incorrect
+            header('Location: ./?action=connexion'); // utilisateur ou mot de passe incorrect
         }
     }
     else
     {
-       header('Location: ./?action=connexion?erreur=2'); // utilisateur ou mot de passe vide
+        header('Location: ./?action=connexion'); // utilisateur ou mot de passe vide
     }
 }
 else
 {
-   header('Location: ./?action=connexion');
+    header('Location: ./?action=connexion');
 }
 ?>
