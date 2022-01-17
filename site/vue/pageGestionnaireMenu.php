1552c14834f6e88ecf9bd95f70f48de64cbc00e3
THYtoon72 <luca.roba@gastonberger.fr>
2022-01-17 14:42:28 +0100

    sla

---
+++ site/vue/pageGestionnaireMenu.php
@@ -0,0 +1,17 @@
+<body>
+    <nav>
+        <ul>
+            <li>CashCash</li>
+            <li>Gestionnaire</li>
+        </ul>
+    </nav>
+    <h1>Page menu</h1>
+    
+    <button class="Gestionnaire" type="button" href="outilStatistique.com"> Outil Statistique
+    </button>
+
+    <button class="Gestionnaire" type="button" href="affectationVisite.com" > Affectation Visite
+    </button>
+
+
+</body>
