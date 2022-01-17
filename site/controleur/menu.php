9345c4733ca7564d8002904bf59101ba66ef8d1b
Leo <debonnetleo4@gmail.com>
2022-01-17 14:14:29 +0100

    erreur

--- site/controleur/menu.php
+++ site/controleur/menu.php
@@ -2,13 +2,13 @@
 
 session_start();
 
-include_once "../modele/menu.inc.php";
+include_once "$racine/modele/menu.inc.php";
 $travail=GetEmploie();
 
 var_dump($travail);
-var_dump($travail['matricule']);
+var_dump($travail["matricule"]);
 
-    if ($_SESSION['matricule'] != $travail['matricule']){  // nom d'utilisateur vérification
+    if(isset($_SESSION["matricule"]) && $_SESSION["matricule"] != $travail["matricule"]){  // nom d'utilisateur vérification
         for($i = 0; $i<count($travail); $i++){
         
         }
