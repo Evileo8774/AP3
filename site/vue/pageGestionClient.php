ba102d3e242144236cd44b6380b49a6921fd0922
Valentin-gastonberger <91471804+Valentin-gastonberger@users.noreply.github.com>
2022-01-17 16:35:05 +0100

    page client

--- site/vue/pageGestionClient.php
+++ site/vue/pageGestionClient.php
@@ -27,10 +27,30 @@
         ?>
         <div class="client">
             <div class="numClient">
-                fiche n°<?= $clients[$i]["numClient"] ?>
+                Client n°<?= $clients[$i]["numClient"] ?>
             </div>
             <div class="derouler">
                 <img src="../images/arrow.png" alt="arrow" class="arrowDeploy">
+            </div>
+        </div>
+        <div class="fiche">
+            <div class="nomClient">
+                Nom client : <?= $clients[$i]["raisonSociale"] ?>
+            </div>
+            <div class="infoClient">
+                <div class="numClient">
+                    Num Client : <?= $clients[$i]["numClient"] ?>
+                </div>
+                <div class="telClient">
+                    Numéro de téléphone : <?= $clients[$i]["tel"] ?> 
+                </div>
+                <div class="mailClient">
+                    Adresse mail : <?= $clients[$i]["email"] ?>
+                </div>
+            </div>
+            <div class="adresse">
+                Adresse : <?= $clients[$i]["adresse"] ?>
+                <div class="map"><iframe width="40%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=<?=$clients[$i]["adresse"]?>&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">j'ai fais ça lol</a></iframe></div>
             </div>
         </div>
         <?php
