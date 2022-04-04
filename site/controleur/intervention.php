0bb87c2c729f0c7cdf19776bc504d057acb961ca
THYtoon72 <91471928+THYtoon72@users.noreply.github.com>
2022-04-04 15:14:08 +0200

    Co-authored-by: Valentin-gastonberger <Valentin-gastonberger@users.noreply.github.com>

--- site/controleur/intervention.php
+++ site/controleur/intervention.php
@@ -3,8 +3,23 @@
 include_once "$racine/modele/intervention.inc.php";
 
     $techniciens = getTechniciens();
+    $employe = getEmployes();
     $clients = getClients();
 
+    if(isset($_GET["Modifier"])
+
+    )
+
+
+    if(isset($_POST["nomClient"])){
+        $nomClient = $_POST["nomClient"];
+    }
+    if(isset($_POST["numClient"])){
+        $nomClient = $_POST["numClient"];
+    }
+    if(isset($_POST["nomClient"])){
+        $nomClient = $_POST["nomClient"];
+    }
 
     $titre = "intervention";
     include "$racine/vue/entete.html.php";
--- site/modele/intervention.inc.php
+++ site/modele/intervention.inc.php
@@ -22,6 +22,26 @@
         return $resultat;
     }
 
+    function getEmployes() {
+        $resultat = array();
+    
+        try {
+            $cnx = connexionPDO();
+            $req = $cnx->prepare("select * from employe");
+            $req->execute();
+    
+            $ligne = $req->fetch(PDO::FETCH_ASSOC);
+            while ($ligne) {
+                $resultat[] = $ligne;
+                $ligne = $req->fetch(PDO::FETCH_ASSOC);
+            }
+        } catch (PDOException $e) {
+            print "Erreur !: " . $e->getMessage();
+            die();
+        }
+        return $resultat;
+    }
+
     function getClients() {
         $resultat = array();
     
@@ -42,4 +62,71 @@
         return $resultat;
     }
 
+
+   
+
+        function getModif($array,$table,  ){
+        
+            $count = 0;
+    
+            try{
+                $cnx = connectionPDO();
+    
+                //on ajoute 1 au compteur si une valeur est entrée
+    
+                for($i = 0; $i < sizeof($array); $i++){
+                    if($array[$i] == "roleIntervenant"){
+                        $index = $i+1;
+                    }
+                    $count++;
+                }
+    
+                $count /= 2;
+                
+
+                param : numClient, dateVisite, matricule
+                //en fonction du compteur on allonge la requête sql
+                
+                switch ($count) {
+                    case 1:
+                        $sql = "UPDATE $table SET ".$array[0]." = :a WHERE mailIntervenant like :mail";
+                        $stmt = $cnx->prepare($sql);
+                        if($array[1] == $array[$index]) $stmt->bindValue(':a', $array[1], PDO::PARAM_INT);
+                        else $stmt->bindValue(':a', $array[1], PDO::PARAM_STR);
+    
+                        $stmt->bindValue(':mail', $mailAncien, PDO::PARAM_STR);
+                        $stmt->execute();
+                        break;
+                       
+                    case 2:
+                        $sql = "UPDATE $table SET ".$array[0]." = :a WHERE mailIntervenant like :mail";
+                        $stmt = $cnx->prepare($sql);
+                        if($array[1] == $array[$index]) $stmt->bindValue(':a', $array[1], PDO::PARAM_INT);
+                        else $stmt->bindValue(':a', $array[1], PDO::PARAM_STR);
+    
+                        if($array[3] == $array[$index]) $stmt->bindValue(':b', $array[3], PDO::PARAM_INT);
+                        else $stmt->bindValue(':b', $array[3], PDO::PARAM_STR);
+    
+                        $stmt->bindValue(':mail', $mailAncien, PDO::PARAM_STR);
+                        $stmt->execute();
+                        break;
+                         
+                    case 3:
+                        $sql = "UPDATE $table SET ".$array[0]." = :a WHERE mailIntervenant like :mail";
+                        $stmt = $cnx->prepare($sql);
+                        if($array[1] == $array[$index]) $stmt->bindValue(':a', $array[1], PDO::PARAM_INT);
+                        else $stmt->bindValue(':a', $array[1], PDO::PARAM_STR);
+    
+                        if($array[3] == $array[$index]) $stmt->bindValue(':b', $array[3], PDO::PARAM_INT);
+                        else $stmt->bindValue(':b', $array[3], PDO::PARAM_STR);
+    
+                        if($array[5] == $array[$index]) $stmt->bindValue(':c', $array[5], PDO::PARAM_INT);
+                        else $stmt->bindValue(':c', $array[5], PDO::PARAM_STR);
+    
+                        
+                        $stmt->bindValue(':mail', $mailAncien, PDO::PARAM_STR);
+                        $stmt->execute();
+                        break; 
+
+    }
 ?>
