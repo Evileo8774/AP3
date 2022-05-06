6af82be2a3933cc9741d47e195718ecba1439c5e
Leo <54958225+Evileo8774@users.noreply.github.com>
2022-05-06 16:57:15 +0200

    Create utilisateur.sql

---
+++ bdd/utilisateur.sql
@@ -0,0 +1,5 @@
+CREATE USER 'defaultUser'@'localhost' IDENTIFIED BY 'g4e5h4er5g';
+GRANT SELECT ON ap3.* TO 'defaultUser'@'localhost';
+GRANT INSERT ON ap3.* TO 'defaultUser'@'localhost';
+GRANT UPDATE ON ap3.* TO 'defaultUser'@'localhost';
+GRANT DELETE ON ap3.* TO 'defaultUser'@'localhost';
